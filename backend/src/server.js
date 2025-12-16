989a4bd0a8611e9e7ed66a14967c239ea7640fc0
Phung Duc Long <phungduclong268@gmail.com>
2025-12-16 15:11:55 +0700

    deloy l2

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,11 +29,11 @@ app.use("/api/tasks", taskRouter)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("/.*/", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend/dist/index.html")
-    )
-  })
+  app.get(/.*/, (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/dist/index.html")
+  )
+})
 }
 
 // Start server sau khi connect DB
